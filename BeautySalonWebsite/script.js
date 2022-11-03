625acf8991a98a0c8b457d247b661efe91fa6da4
jocelinegarcia <42221352+jocelinegarcia@users.noreply.github.com>
2022-11-02 22:35:55 -0400

    Delete BeautySalonWebsite directory

--- BeautySalonWebsite/script.js
+++
@@ -1,6 +0,0 @@
-/*HOME JS- Event listener for hamburger menu */
-document.querySelector(".hamburger-menu").addEventListener("click", () => {
-    document.querySelector(".container").classList.toggle("change");
-  });
-  
-

7994c264e6346cf851469bf7e97035559fa97608
jocelinegarcia <42221352+jocelinegarcia@users.noreply.github.com>
2022-11-02 22:36:11 -0400

    Add files via upload

---
+++ BeautySalonWebsite/script.js
@@ -0,0 +1,6 @@
+/*HOME JS- Event listener for hamburger menu */
+document.querySelector(".hamburger-menu").addEventListener("click", () => {
+    document.querySelector(".container").classList.toggle("change");
+  });
+  
+
